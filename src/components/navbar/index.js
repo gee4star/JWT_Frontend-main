076278a42a88d4926108cdceb5e9648fac9507b3
Your Usernameclear <151780860+gee4star@users.noreply.github.com>
2024-03-26 08:15:14 -0700

    Sample Recipe Page Works

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav>
       <Link className="link" to='/'>Home</Link>
       <Link className="link" to='/auth'>Login</Link>
-      <Link className="link" to='/recipebook'>Recipe Book</Link>
+      <Link className="link" to='/recipebook'>Sample Recipe</Link>
       <Link className="link" to='/dashboard'>Personal Recipe List</Link>
     </nav>
   );
--- src/pages/recipebook/index.js
+++ src/pages/recipebook/index.js
@@ -1,4 +1,8 @@
 // import { useAuth } from '../../contexts/auth/auth_context';
+// import dotenv from 'dotenv';
+import React, {useState, useEffect} from "react"
+const apiKeySpring = process.env.apiKey || "f9cabc1a80b4416eab94bd0e6c909f3b"
+const apiKey = apiKeySpring.replace(/"/g, '');
 const products = [
   {
     id: 1,
@@ -12,6 +16,13 @@ const products = [
   // More products...
 ]
 const RecipeBook = () => {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&include-tags=vegetarian`)
+    .then(response => response.json())
+    .then(data => setRecipes(data.recipes))
+  }, [])
   //   const { logout } = useAuth();
 
   //   const handleClick = () => {
@@ -20,8 +31,14 @@ const RecipeBook = () => {
 
   return (
     <div>
-      <h1>This is the Recipe Book</h1>
-      
+      <h1>Sample Recipes</h1>
+      { recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3> {recipe.title}</h3>
+          <img src={recipe.image}/>
+          <span>{recipe.summary}</span>
+        </div>
+      ))}
     </div>
   );
 };
